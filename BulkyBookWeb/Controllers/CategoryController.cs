6494ea51671a972d3565cbcb079512351bc31ac4
Francesco <fra.boasso@gmail.com>
2022-05-25 16:40:37 +0200

    Aggiunto Edit

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -42,4 +42,66 @@ public class CategoryController : Controller
         }
         return View(obj);
     }
+    // GET
+    public IActionResult Edit(int? Id)
+    {
+        if (Id == null || Id == 0)
+        {
+            return NotFound();
+        }
+        var categoryFromDb = _db.Categories.Find(Id);
+        // var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id==Id);
+        // var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id==Id);
+        if (categoryFromDb == null)
+        {
+            return NotFound();
+        }
+        return View(categoryFromDb);
+    }
+    // POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Category obj)
+    {
+        if(obj.Name == obj.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("name","The DisplayOrder cannot exactly match the Name.");
+        }
+        if (ModelState.IsValid){            
+            _db.Categories.Update(obj);        
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View(obj);
+    }
+    // GET
+    public IActionResult Delete(int? Id)
+    {
+        if (Id == null || Id == 0)
+        {
+            return NotFound();
+        }
+        var categoryFromDb = _db.Categories.Find(Id);
+        // var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id==Id);
+        // var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id==Id);
+        if (categoryFromDb == null)
+        {
+            return NotFound();
+        }
+        return View(categoryFromDb);
+    }
+    // POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Delete(int? Id)
+    {
+        var obj = _db.Categories.Find(Id);
+        if(obj == null)
+        {
+            return NotFound();
+        }        
+            _db.Categories.Update(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+    }
 }
